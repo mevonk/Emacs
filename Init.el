4d174d613aaa6f88bc90c8b805bab327ae659d35
mevonk <113317856+mevonk@users.noreply.github.com>
2026-04-08 18:44:16 -0400

    Update Init.el

--- Init.el
+++ Init.el
@@ -8,7 +8,7 @@
 (defun crafted-emacs-load-modules (modules)
   "Initialize crafted-emacs modules.
 
-MODULES is a list of module names without the -packages or
+MODULES is a list of module names without the -packages or 
 -config suffixes.  Note that any user-provided packages should be
 added to `package-selected-packages' before invoking this
 function."
@@ -17,3 +17,9 @@ function."
   (package-install-selected-packages :noconfirm)
   (dolist (m modules)
     (require (intern (format "crafted-%s-config" m)) nil :noerror)))
+
+;; Any extra packages in addition to the ones added by crafted-emacs
+;; modules go here before we call `crafted-emacs-load-modules'.
+(customize-set-variable 'package-selected-packages '(ef-themes magit))
+
+(crafted-emacs-load-modules '(defaults completion ui ide))
